115609bd5d2a512b1b842a81ca2abe1b641b2a11
freddypauls <paasan-fred@hotmail.com>
2018-04-01 22:58:43 +0200

    login starting to take shape

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -1,1 +1,21 @@
 //window.alert("Hello");
+
+var textField = document.getElementById("last_name");
+
+function testFunc() {
+	
+	var ref = firebase.database().ref();
+	
+	var messsage = textField.value;
+	
+	ref.push().set(messsage);
+	
+}
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+		
+  } else {
+    // No user is signed in.
+  }
+});
---
+++ webapp/js/login.js
@@ -0,0 +1,17 @@
+$("#loginBtn").click(
+	function(){
+		
+		var email = $("#loginEmail").val();
+		var password = $("#loginPassword").val();
+
+		if(email != "" && password != ""){
+			
+			firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+				
+				$("#loginError").show().text(error.message);
+				
+			});
+			
+		}
+		
+});
